b7aa00b7d1d06668c6eeb27cc53d865b3253dee0
wad2601hfh <wad2601hfh@gmail.com>
2026-01-27 11:06:25 +0700

    addition

--- backend/utils.php
+++ backend/utils.php
@@ -1,22 +1,34 @@
 <?php
 // backend/utils.php
 
+function getPath($filename) {
+    // This ensures we look relative to THIS file (utils.php), 
+    // solving directory issues on Vercel.
+    return __DIR__ . '/' . $filename;
+}
+
 function readJson($filename) {
-    if (!file_exists($filename)) return [];
-    $content = file_get_contents($filename);
+    $path = getPath($filename); // Use absolute path
+    if (!file_exists($path)) return [];
+    $content = file_get_contents($path);
     return json_decode($content, true) ?? [];
 }
 
 function writeJson($filename, $data) {
-    // Safe write with file locking
-    $fp = fopen($filename, 'c+');
-    if (flock($fp, LOCK_EX)) {
-        ftruncate($fp, 0);
-        fwrite($fp, json_encode($data, JSON_PRETTY_PRINT));
-        fflush($fp);
-        flock($fp, LOCK_UN);
+    $path = getPath($filename); // Use absolute path
+    
+    // NOTE: On Vercel, this might fail or not persist.
+    // We suppress errors with @ for the demo.
+    $fp = @fopen($path, 'c+');
+    if ($fp) {
+        if (flock($fp, LOCK_EX)) {
+            ftruncate($fp, 0);
+            fwrite($fp, json_encode($data, JSON_PRETTY_PRINT));
+            fflush($fp);
+            flock($fp, LOCK_UN);
+        }
+        fclose($fp);
     }
-    fclose($fp);
 }
 
 function jsonResponse($data) {
